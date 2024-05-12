76fcdacbd15caaf7898dba68049ba0a79f8a9fea
ahmed-nt <ahmed.elataallah@gmail.com>
2024-05-12 10:26:42 +0300

    ADD check middleware verify-token

--- backend/middlewares/checkToken.js
+++ backend/middlewares/checkToken.js
@@ -5,10 +5,13 @@ import User from "#models/user.js";
 
 export default eah(async function (rq, rs, nx) {
     const token = rq.headers["authorization"];
+    console.log("token",token)
     if (!token) return nx(error("token not provided"));
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    console.log("decodedToken",decodedToken)
         const user = await User.findById(decodedToken._id);
+    console.log("user",user)
         if (!user) return nx(error("user not found with this token", 404));
         rq.user = user;
         nx();
